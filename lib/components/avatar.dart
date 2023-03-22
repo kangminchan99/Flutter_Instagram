6e01844554878c0ae2605c73e71d5700073009e8
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-03-22 21:42:51 +0900

    my_image

--- lib/components/avatar.dart
+++ lib/components/avatar.dart
@@ -12,37 +12,42 @@ class Avatar extends StatelessWidget {
   Avatar(
       {this.hasStroy,
       this.nickname,
-      this.size,
+      // 디폴트 값 정의
+      this.size = 65,
       required this.thumbPath,
       required this.type,
       super.key});
   // 아바타 뒤에 그라데이션이 들어가야하는 위젯
   Widget type1Widget() {
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 5),
-      // 아바타 테두리 보이게
-      padding: const EdgeInsets.all(2),
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: [Colors.purple, Colors.orange]),
-        shape: BoxShape.circle,
-      ),
-      child: Container(
-        decoration:
-            const BoxDecoration(shape: BoxShape.circle, color: Colors.white),
-        // 아바타 테두리 흰색
+        margin: const EdgeInsets.symmetric(horizontal: 5),
+        // 아바타 테두리 보이게
         padding: const EdgeInsets.all(2),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(65),
-          child: Container(
-            width: 65,
-            height: 65,
-            child: CachedNetworkImage(
-              imageUrl: thumbPath,
-              fit: BoxFit.cover,
-            ),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.purple, Colors.orange]),
+          shape: BoxShape.circle,
+        ),
+        child: type2Widget());
+  }
+
+  // 내 프로필 아바타
+  Widget type2Widget() {
+    return Container(
+      decoration:
+          const BoxDecoration(shape: BoxShape.circle, color: Colors.white),
+      // 아바타 테두리 흰색
+      padding: const EdgeInsets.all(2),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(size!),
+        child: SizedBox(
+          width: size,
+          height: size,
+          child: CachedNetworkImage(
+            imageUrl: thumbPath,
+            fit: BoxFit.cover,
           ),
         ),
       ),
@@ -54,8 +59,8 @@ class Avatar extends StatelessWidget {
     switch (type) {
       case AvatarType.TYPE1:
         return type1Widget();
-        break;
       case AvatarType.TYPE2:
+        return type2Widget();
       case AvatarType.TYPE3:
         return Container();
         break;
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,16 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_insta/components/avatar.dart';
 import 'package:flutter_insta/components/image_data.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
+
+  // 내 썸네일과  + 모양이 겹쳐있으므로 스택을 사용해야 한다.
+  Widget _myStory() {
+    return Stack(
+      children: [
+        Avatar(
+            thumbPath:
+                'https://thumbnews.nateimg.co.kr/view610///news.nateimg.co.kr/orgImg/hk/2019/08/02/03.20228245.1.jpg',
+            size: 70,
+            type: AvatarType.TYPE2),
+        // 내 프로필 아래에 플러스 추가하기
+        Positioned(
+            right: 5,
+            bottom: 0,
+            child: Container(
+              width: 25,
+              height: 25,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue,
+                  border: Border.all(color: Colors.white, width: 2)),
+              child: const Center(
+                child: Text(
+                  '+',
+                  style:
+                      TextStyle(color: Colors.white, fontSize: 20, height: 1.1),
+                ),
+              ),
+            ))
+      ],
+    );
+  }
+
+  // 중앙 포스트 만들기
+  Widget _postList() {
+    return 
+  }
 
   // 아바타 영역 구현
   Widget _storyBoardList() {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
-      child: Row(
-        children: List.generate(
+      child: Row(children: [
+        const SizedBox(width: 20),
+        // ... - List 배열을 나열 하겠다는 의미
+        _myStory(),
+        const SizedBox(width: 5),
+        ...List.generate(
           100,
           (index) => Avatar(
             type: AvatarType.TYPE1,
@@ -18,7 +60,7 @@ class Home extends StatelessWidget {
                 'https://cdn.9oodnews.com/news/photo/202201/13232_19161_1443.jpg',
           ),
         ),
-      ),
+      ]),
     );
   }
 
@@ -45,7 +87,7 @@ class Home extends StatelessWidget {
         ],
       ),
       body: ListView(
-        children: [_storyBoardList()],
+        children: [_storyBoardList(), _postList()],
       ),
     );
   }
