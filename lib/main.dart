d98960038f2774e0c493c66c87c98ecb80b591e9
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-03-31 21:21:12 +0900

    firebase_setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_insta/firebase_options.dart';
 import 'package:flutter_insta/utils/app.dart';
 import 'package:flutter_insta/utils/init_binding.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
